844fe7bae3fb5d1bda5488db2d9bdf29e5fa2daa
lmlearning <l.malmqvist@gmail.com>
2026-03-10 08:33:27 +0100

    Add main.jsx to render AlgebraicTricks component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AlgebraicTricks from './AlgebraicTricks.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AlgebraicTricks />
+  </React.StrictMode>,
+)
